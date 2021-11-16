dab74917e36a9977fd06fee3f07aaf8b9ceb56e8
DuZzzs <duzchn@gmail.com>
2021-11-16 09:22:39 +0000

    [update] add pose_resnet

--- experiments/example/train.sh
+++ experiments/example/train.sh
@@ -4,7 +4,8 @@ CONFIG=$1
 
 # train
 #python ../../tools/train_val.py --config ${CONFIG}
-python ../../tools/train_val.py --config test_kitti.yaml
+#python ../../tools/train_val.py --config test_kitti.yaml
+python ../../tools/train_val.py --config test_kitti_res18.yaml
 
 # test
 #python ../../tools/train_val.py --config test_kitti.yaml --e
